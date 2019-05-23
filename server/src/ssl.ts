5f61235efe1bd8ac161f7b05736f52fd14776133
Vijon <mauro@vijon.it>
2019-05-23 16:03:46 +0200

    ssl port

--- server/src/ssl.ts
+++ server/src/ssl.ts
@@ -8,7 +8,7 @@ export default function(app) {
             cert: fs.readFileSync(app.get('ssl')['cert'])
         };
 	    
-        const server = https.createServer(opts, app);
+        const server = https.createServer(opts, app).listen(app.get('port'));
         app.setup(server);
     }
 }
