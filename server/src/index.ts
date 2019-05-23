8525d56f789ae4fdc2e685584b49bf5d4059397f
Vijon <mauro@vijon.it>
2019-05-23 16:55:09 +0200

    fixed https listen, fixed manifesto

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,7 @@
-import logger from './logger';
 import app from './app';
-
-const port = app.get('port');
-const server = app.listen(port);
+import logger from './logger';
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
 );
 
-server.on('listening', () =>
-  logger.info('Feathers application started on https://%s:%d', app.get('host'), port)
-);
--- server/src/ssl.ts
+++ server/src/ssl.ts
@@ -1,14 +1,22 @@
 import * as https from "https";
 import * as fs from "fs";
+import logger from './logger';
 
 export default function(app) {
+    const port = app.get('port');
+    var server;
     if (app.get('ssl')) {
         const opts = {
             key: fs.readFileSync(app.get('ssl')['key']),
             cert: fs.readFileSync(app.get('ssl')['cert'])
         };
 	    
-        const server = https.createServer(opts, app);
+        server = https.createServer(opts, app).listen(port);
         app.setup(server);
+    } else {
+        server = app.listen(port);
     }
+    server.on('listening', () =>
+        logger.info('Feathers application started on https://%s:%d', app.get('host'), port)
+    );
 }
