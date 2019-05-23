24753dc43a93035fdc300fc9b67d78554f63b8ee
Ubuntu <ubuntu@ip-172-26-15-32.eu-central-1.compute.internal>
2019-05-23 13:27:56 +0000

    ssl fix on prod

--- server/src/ssl.ts
+++ server/src/ssl.ts
@@ -1,13 +1,13 @@
 import * as https from "https";
+import * as fs from "fs";
 
 export default function(app) {
     if (app.get('ssl')) {
         const opts = {
-            key: app.get('ssl')['key'],
-            cert: app.get('ssl')['cert']
+            key: fs.readFileSync(app.get('ssl')['key']),
+            cert: fs.readFileSync(app.get('ssl')['cert'])
         };
-        console.log(opts)
-    
+	    
         const server = https.createServer(opts, app);
         app.setup(server);
     }
